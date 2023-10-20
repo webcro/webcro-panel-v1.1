1951946803acc4007e40723163969210482dda6d
webcro <gabriel273274@gmail.com>
2023-10-20 21:11:54 +0000

    modify config.js

--- config/config.js
+++ config/config.js
@@ -2,6 +2,11 @@ const SERVER_A_IP = process.env.SERVER_A_IP || 'localhost'; // Default to 'local
 const SERVER_A_PORT = process.env.SERVER_A_PORT || '3000';  // Default to '4000' if not set
 const SERVER_A = `http://${SERVER_A_IP}:${SERVER_A_PORT}`;
 
+const SERVER_B_IP = process.env.SERVER_B_IP || 'localhost'; // Default to 'localhost' if not set
+const SERVER_B_PORT = process.env.SERVER_B_PORT || '3000';  // Default to '4000' if not set
+const SERVER_B = `http://${SERVER_B_IP}:${SERVER_B_PORT}`;
+
 module.exports = {
-    SERVER_A
+    SERVER_A,
+    SERVER_B
 };
